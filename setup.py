70f075ebd6c83a120c4d5bd6275f40adb41a7624
Brandon Carpenter <brandon@8minute.com>
2020-07-09 15:58:44 -0700

    Build SSC from source
    NREL isn't distributing the SSC API via a zip file and the libraries in
    the SAM distribution are insufficient (at least on Windows), so SSC must
    built from scratch, which isn't too difficult.

--- setup.py
+++ setup.py
@@ -1,115 +1,257 @@
 """Setup script for samlib"""
-
-from distutils.command.build_ext import build_ext as _build_ext
-from distutils.dep_util import newer_group
-from distutils import log, spawn
-import math
+import os
 import pathlib
 import shutil
+import subprocess
 import sys
-import zipfile
+import tarfile
 
-from setuptools import Command, find_packages, setup
-
-
-SSC_REVISION = 209
-SSC_VERSION = '2018.11.11.r4'
-SSC_ZIP_NAME = f'sam-sdk-{SSC_REVISION}_{"_".join(SSC_VERSION.rsplit(".", 1))}.zip'
-SSC_URL = f'https://github.com/NREL/ssc/releases/download/{SSC_VERSION}/{SSC_ZIP_NAME}'
-SSC_ZIP = pathlib.Path('sam-sdk', SSC_ZIP_NAME)
+import setuptools
+from distutils.command.build_ext import build_ext as _build_ext
+from distutils.dep_util import newer
+from distutils.util import get_platform
+from distutils import errors
+from distutils import log
 
 
-class download(Command):
-    description = 'download the SSC library'
+SAM_VERSION = '2020.2.29.r2'
+SSC_REVISION = 240
+SSC_BASENAME = f'{SAM_VERSION}.ssc.{SSC_REVISION}'
+SSC_DIRNAME = f'ssc-{SSC_BASENAME}'
+SSC_TARBALL = f'{SSC_BASENAME}.tar.gz'
+SSC_DOWNLOAD_URL = f'https://github.com/NREL/ssc/archive/{SSC_TARBALL}'
+
+
+class download_ssc(setuptools.Command):
+    description = 'download SSC source code'
     user_options = [
+        ('build-base=', 'b',
+         'base directory for build library'),
         ('force', 'f',
-         'download even if the file already exists'),
+         'forcibly build everything (ignore file timestamps)'),
     ]
     boolean_options = ['force']
 
     def initialize_options(self):
+        self.build_base = None
         self.force = None
 
     def finalize_options(self):
         if self.force is None:
-            self.force = False
+            self.force = 0
+        self.set_undefined_options(
+            'build',
+            ('build_base', 'build_base'),
+        )
+        self.tarball_path = pathlib.Path(self.build_base, SSC_TARBALL)
 
     def run(self):
         import requests
 
-        if not self.force and SSC_ZIP.exists():
-            log.debug(f'skipping download; {SSC_ZIP} exists')
+        if self.force or not self.tarball_path.exists():
+            log.info(f'downloading {SSC_DOWNLOAD_URL}')
+            if not self.dry_run:
+                with requests.get(SSC_DOWNLOAD_URL, stream=True) as response:
+                    response.raise_for_status()
+                    response.raw.decode_content = True
+                    parent = self.tarball_path.parent
+                    try:
+                        if not parent.exists():
+                            parent.mkdir(parents=True, exist_ok=True)
+                        with open(self.tarball_path, 'wb') as tarball:
+                            shutil.copyfileobj(response.raw, tarball)
+                    except:
+                        self.tarball_path.unlink()
+                        raise
+
+
+class build_ssc(setuptools.Command):
+    description = 'build SSC library'
+    user_options = [
+        ('build-base=', 'b',
+         'base directory for build library'),
+        ('debug', 'g',
+         "compile with debugging information"),
+        ('force', 'f',
+         "forcibly build everything (ignore file timestamps)"),
+        ('parallel=', 'j',
+         "number of parallel build jobs"),
+    ]
+    boolean_options = ['debug', 'force']
+    sub_commands = [
+        ('download_ssc', lambda _: True),
+    ]
+
+    def initialize_options(self):
+        self.build_base = None
+        self.debug = None
+        self.force = None
+        self.parallel = None
+
+    def finalize_options(self):
+        self.set_undefined_options(
+            'build_ext',
+            ('build_base', 'build_base'),
+            ('debug', 'debug'),
+            ('force', 'force'),
+            ('parallel', 'parallel'),
+        )
+        if isinstance(self.parallel, str):
+            try:
+                self.parallel = int(self.parallel)
+            except ValueError:
+                raise errors.DistutilsOptionError("parallel should be an integer")
+        self.build_path = pathlib.Path(self.build_base, 'ssc', SSC_DIRNAME)
+        self.source_path = pathlib.Path(self.build_base, 'src', SSC_DIRNAME)
+        self.tarball_path = pathlib.Path(self.build_base, SSC_TARBALL)
+
+    def extract(self):
+        if self.force or not (self.source_path/'CMakeLists.txt').exists():
+            if not self.tarball_path.exists():
+                self.run_command('download_ssc')
+            log.info(f'extracting {self.tarball_path}')
+            if not self.dry_run:
+                with tarfile.open(self.tarball_path) as tarball:
+                    tarball.extractall(self.source_path.parent)
+
+    def spawn(self, cmd, **kwargs):
+        log.info(' '.join(cmd))
+        if self.dry_run:
             return
-        log.info(f'downloading {SSC_URL}')
-        if not self.dry_run:
-            with requests.get(SSC_URL, stream=True) as response, SSC_ZIP.open('wb') as dst:
-                response.raw.decode_content = True
-                shutil.copyfileobj(response.raw, dst)
+        rc = subprocess.run(cmd, **kwargs).returncode
+        if rc:
+            sys.exit(rc)
+
+    def cmake(self, *additional_args, env=None):
+        if not self.build_path.exists():
+            self.build_path.mkdir(0o755, parents=True)
+        self.spawn(['cmake', *additional_args, '-Dskip_tools=1', '-Dskip_tests=1',
+                    str(self.source_path.absolute())], cwd=self.build_path, env=env)
+        jobs = (f'-j{self.parallel}',) if self.parallel else ()
+        self.spawn(['cmake', '--build', str(self.build_path), *jobs,
+                    '--config', 'Debug' if self.debug else 'Release', '--target', 'ssc'], env=env)
+
+    def build_linux(self):
+        self.cmake(f'-DCMAKE_BUILD_TYPE={"Debug" if self.debug else "Release"}', '-DSAMAPI_EXPORT=1')
+
+    def build_macos(self):
+        self.cmake(f'-DCMAKE_BUILD_TYPE={"Debug" if self.debug else "Release"}')
+        source = self.build_path/'ssc/ssc.dylib'
+        target = self.build_path/'ssc/libssc.dylib'
+        if newer(source, target):
+            shutil.copy(source, target)
+            try:
+                self.spawn(['install_name_tool', '-id', '@loader_path/libssc.dylib', str(target)])
+            except:
+                target.unlink()
+                raise
+
+    def build_windows(self):
+        env = {**os.environ, 'SAMNTDIR': str(self.build_path.absolute())}
+        (self.build_path/'deploy/x64').mkdir(0o755, parents=True, exist_ok=True)
+        self.cmake('-G', 'Visual Studio 16 2019', '-DCMAKE_CONFIGURATION_TYPES=Debug;Release',
+                   '-DCMAKE_SYSTEM_VERSION=10.0', '-Dskip_api=1', env=env)
+
+    def run(self):
+        self.extract()
+        build = {
+            'cygwin': self.build_windows,
+            'win32': self.build_windows,
+            'darwin': self.build_macos,
+        }.get(sys.platform, self.build_linux)
+        print('building ssc library')
+        build()
 
 
 class build_ext(_build_ext):
-    sub_commands = [
-        ('download', lambda _: True),
+    user_options = [
+        ('build-base=', 'b',
+         'base directory for build library'),
+        *_build_ext.user_options,
     ]
+    sub_commands = [
+        ('build_ssc', lambda _: True),
+    ]
+
+    def initialize_options(self):
+        super().initialize_options()
+        self.build_base = None
+
+    def finalize_options(self):
+        super().finalize_options()
+        self.set_undefined_options(
+            'build',
+            ('build_base', 'build_base'),
+        )
+        self.lib_name = 'sscd' if self.debug else 'ssc'
+        self.lib_path = pathlib.Path(self.build_base, 'ssc', SSC_DIRNAME, 'ssc')
+        if sys.platform == 'win32':
+            self.lib_path /= 'Debug' if self.debug else 'Release'
+        self.src_path = pathlib.Path(self.build_base, 'src', SSC_DIRNAME, 'ssc')
 
     def run(self):
         for cmd in self.get_sub_commands():
             self.run_command(cmd)
-        self.extract_ssc()
+        self.setup_ssc()
         super().run()
+        self.copy_ssc()
 
-    def extract_ssc(self):
-        bits = int(math.log2(sys.maxsize) + 1)
+    def read_sscapi(self):
+        source = []
+        with (self.src_path/'sscapi.h').open() as file:
+            for line in file:
+                if line.startswith('#endif // __SSCLINKAGECPP__'):
+                    break
+            for line in file:
+                if line.startswith('#ifndef __SSCLINKAGECPP__'):
+                    break
+                if line.startswith('SSCEXPORT '):
+                    line = line[10:]
+                source.append(line)
+        source.append(r"""
+extern "Python" ssc_bool_t _handle_update(ssc_module_t module, ssc_handler_t handler,
+       int action, float f0, float f1, const char *s0, const char *s1, void *user_data);
+    """)
+        return ''.join(source)
+
+    def setup_ssc(self):
+        import cffi
+
+        extension, = self.extensions
+        ffibuilder = cffi.FFI()
+        ffibuilder.cdef(self.read_sscapi())
+        ffibuilder.set_source(extension.name, '#include "sscapi.h"', libraries=[self.lib_name],
+                              include_dirs=[str(self.src_path)], library_dirs=[str(self.lib_path)],
+                              extra_link_args=(['-Wl,-rpath=${ORIGIN}'] if sys.platform == 'linux' else []))
+        if not self.dry_run:
+            self.extensions = [ffibuilder.distutils_extension(self.build_temp, self.verbose)]
+
+    def copy_ssc(self):
+        ssc = self.lib_name
         if sys.platform in ['win32', 'cygwin']:
-            platform = 'win'
-            lib_name = 'ssc.dll'
+            lib_name = f'{ssc}.dll'
         elif sys.platform == 'darwin':
-            platform = 'osx'
-            lib_name = 'libssc.dylib'
+            lib_name = f'lib{ssc}.dylib'
         else:
-            platform = 'linux'
-            lib_name = 'libssc.so'
-        sdk_path = pathlib.Path(self.build_temp, 'sam-sdk')
-        self.library_dirs.append(str(sdk_path))
-        lib_path = sdk_path / lib_name
-        if self.force or newer_group([SSC_ZIP], lib_path, 'newer'):
-            log.debug(f'extracting SDK to {sdk_path}')
-            if not self.dry_run:
-                sdk_path.mkdir(0o755, True, True)
-                dirname = f'{platform}{bits}/'
-                with zipfile.ZipFile(SSC_ZIP) as file:
-                    for info in file.filelist:
-                        if not info.filename.startswith(dirname) or info.is_dir():
-                            continue
-                        name = info.filename.split('/')[-1]
-                        if name in ['ssc.so', 'ssc.dylib']:
-                            name = f'lib{name}'
-                        with file.open(info, 'r') as src, (sdk_path / name).open('wb') as dst:
-                            shutil.copyfileobj(src, dst)
-                if platform != 'win':
-                    lib_path.chmod(0o755)
-                if sys.platform == 'darwin':
-                    spawn.spawn(['install_name_tool', '-id', '@loader_path/libssc.dylib', str(lib_path)])
-        libssc_path = pathlib.Path('.' if self.inplace else self.build_lib, 'samlib', lib_name)
-        if self.force or newer_group([lib_path], libssc_path, 'newer'):
-            if not self.dry_run:
-                shutil.copyfile(lib_path, libssc_path)
-                if platform != 'win':
-                    libssc_path.chmod(0o755)
+            lib_name = f'lib{ssc}.so'
+        src = self.lib_path/lib_name
+        dst = pathlib.Path('.' if self.inplace else self.build_lib, 'samlib', lib_name)
+        if newer(src, dst):
+            shutil.copy(src, dst)
 
 
 with open('README.md') as file:
     long_description = file.read()
 
 
-setup(
+setuptools.setup(
     name='samlib',
-    version='0.1.3.{}'.format(SSC_REVISION),
+    version='0.2.0.{}'.format(SSC_REVISION),
     python_requires='>=3.6',
 
-    packages=find_packages(include=['samlib']),
+    packages=setuptools.find_packages(include=['samlib']),
 
-    cffi_modules=["build_ssc.py:ffibuilder"],
+    ext_modules=[setuptools.Extension('samlib._ssc_cffi', [])],
 
     include_package_data=True,
     package_data={
@@ -126,11 +268,12 @@ setup(
         'cffi>=1.12,<2',
         'requests',
         'setuptools',
+        'wheel',
     ],
 
     author='Brandon Carpenter',
     author_email='brandon@8minute.com',
-    description="High-level library for using NREL's SAM Simulation Core (SSC)",
+    description="High-level library for NREL's SAM Simulation Core (SSC)",
     long_description=long_description,
     long_description_content_type='text/markdown',
     license='BSD',
@@ -156,6 +299,7 @@ setup(
     # Customize extension building to download library
     cmdclass={
         'build_ext': build_ext,
-        'download': download,
+        'build_ssc': build_ssc,
+        'download_ssc': download_ssc,
     }
 )
